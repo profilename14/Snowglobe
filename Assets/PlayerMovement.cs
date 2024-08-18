bb59b0eb27711772ac269fa4c012843817c72bcb
James Jasper Fadden O'Roarke <faddenjasper@gmail.com>
2024-08-17 19:17:35 -0700

    Created Debug level and added death planes

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,17 +1,12 @@
-using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 16f;
+    private float speed = 3f;
+    private float jumpingPower = 24f;
     private bool isFacingRight = true;
-
-    
-    public float fallMultiplier = 3f; // Multiplier to the gravity the player deals with after hitting the peak of their jump
-    public float lowJumpMultiplier = 8f; // If the player doesn't hold jump, multiplier to the extra gravity during before the peak.
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
@@ -22,27 +17,32 @@ public class PlayerMovement : MonoBehaviour
         // Make player always face the direction of gravity
         transform.up = -Physics2D.gravity.normalized;
 
+        // Moving the actual movement to FixedUpdate would be good for optimization later
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().AddForce(transform.right * speed);
+            
+            //transform.Translate(Vector2.right * (Time.deltaTime * speed), Space.Self);
+        }
 
-        horizontal = Input.GetAxis("Horizontal");
-		Vector2 movement = new Vector2(horizontal, 0);
-        Move(movement);
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().AddForce(-transform.right * speed);
 
+            //transform.Translate(Vector2.left * (Time.deltaTime * speed), Space.Self);
+        }
 
+        horizontal = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            rb.velocity = new Vector2(rb.velocity.x, 0);
-            rb.velocity += Vector2.up * jumpingPower;
+            rb.velocity = transform.up * jumpingPower;
         }
-        JumpControl(rb.velocity.y, Input.GetButton("Jump")); // Modifies jumps based on if the button is held or not. Handles gravity!
 
-
-
-
-        /*if (Input.GetButtonUp("Jump") && Vector2.Dot(rb.velocity, transform.up) > 0)
+        if (Input.GetButtonUp("Jump") && Vector2.Dot(rb.velocity, transform.up) > 0)
         {
             rb.velocity = rb.velocity * 0.5f;
-        }*/
+        }
 
         //Flip();
     }
@@ -66,21 +66,6 @@ public class PlayerMovement : MonoBehaviour
             Vector3 localScale = transform.localScale;
             localScale.x *= -1f;
             transform.localScale = localScale;
-        }
-    }
-
-    private void Move(Vector2 movement) {
-        //rb.velocity = Vector2.Lerp(rb.velocity, new Vector2(dir.x * speed, rb.velocity.y), wallJumpLerp * Time.deltaTime);
-		rb.velocity = new Vector2(movement.x * speed, rb.velocity.y);
-    }
-
-    private void JumpControl(float verticalVelocity, bool isJumpHeld) {
-        if(verticalVelocity < 0) // If the player is falling
-        {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-        }else if(verticalVelocity > 0 && !isJumpHeld) // If the player is rising but isn't holding jump
-        {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
     }
 }
